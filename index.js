96a63fcbd9fc245bed40791b50f5796a1e2840f1
onestepforwardacademy-coder <onestepforwardacademy@gmail.com>
2026-01-29 08:14:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function spawnPython(script, args = []) {
 // ------------------------------------------------------------------------------
 // ⚙️ SYSTEM CONFIGURATION & CONSTANTS
 // ------------------------------------------------------------------------------
-const BOT_TOKEN = process.env.TELEGRAM_TOKEN || "8457835043:AAGQb0wtWaDyqZv9g_Hg3ck0a98XGkoii5M";
+const BOT_TOKEN = process.env.TELEGRAM_TOKEN || "8457835043:AAFQhNgdHjRqdCVX3Wf6P1Ef2T4_-NuMUvM";
 const NETWORK = "mainnet-beta";
 const RPC_URL = clusterApiUrl(NETWORK);
 const LOG_FILE = "output.txt";
